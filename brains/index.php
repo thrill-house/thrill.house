d22dcb43de5b641cfd8af73a690ae48f5ab9d9ea
Mike Pierce <info@michaelpierce.trade>
2018-12-21 16:19:07 +0100

    Rename to EMPATIS

--- brains/index.php
+++ brains/index.php
@@ -1,5 +1,5 @@
 <?php
   header('HTTP/1.1 301 Moved Permanently'); 
-  header('Location: /project-empatic' . ($_SERVER['QUERY_STRING']? '?' . $_SERVER['QUERY_STRING']: ''));
+  header('Location: /project-empatis' . ($_SERVER['QUERY_STRING']? '?' . $_SERVER['QUERY_STRING']: ''));
   exit();
 ?>
